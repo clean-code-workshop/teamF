e6d4e5f2990c9c899c251387c2cdcb4055a5bc44
Mischadekker <42527576+Mischadekker@users.noreply.github.com>
2022-12-09 14:19:22 +0100

    Update BuildAndSendMail.java

--- training-assignments/src/java/eu/sig/training/ch05-interfacing/buildandsendmail/BuildAndSendMail.java
+++ training-assignments/src/java/eu/sig/training/ch05-interfacing/buildandsendmail/BuildAndSendMail.java
@@ -2,19 +2,42 @@ package eu.sig.training.ch05.buildandsendmail;
 
 public class BuildAndSendMail {
     // tag::buildAndSendMail[]
-    public void buildAndSendMail(MailMan m, String firstName, String lastName,
-        String division, String subject, MailFont font, String message1,
-        String message2, String message3) {
-        // Format the email address
-        String mId = firstName.charAt(0) + "." + lastName.substring(0, 7) + "@"
-            + division.substring(0, 5) + ".compa.ny";
-        // Format the message given the content type and raw message
-        MailMessage mMessage = formatMessage(font,
-            message1 + message2 + message3);
-        // Send message
-        m.send(mId, subject, mMessage);
+    public void buildAndSendMail (Mailman m, MailAddress mAddress, Mailbody mBody) {
+        // build the mail
+        Mail mail = new Mail(mAddress, mBody);
+        // send the mail (x3)
+        m.sendMail(mail);
     }
-    // end::buildAndSendMail[]
+        
+    prive class Mail {
+        private MailAddress address;
+        private MailBody body;
+        
+        private Mail(MailAddress mAddress, MailBody mBody) {
+            this.address = mAddress;
+            this.body = mBody;
+        }
+    }
+    
+    private class MailBody {
+        String subject;
+        MailmMessage message;
+        
+        public MailBody(String subject, MailMessage message) {
+            this.subject = subject;
+            this.message = message;
+        }
+    }
+    
+    private class MailAddress {
+        private String mId;
+        
+        private MailAddress(String firstname, String lastname, String division) {
+            this.mId = firstName.charAt(0) + "." lastName.substring(0, 7)
+                + "0"
+                + division.substring(0, 5) + ".compa.ny";
+        }
+    )              
 
     @SuppressWarnings("unused")
     private MailMessage formatMessage(MailFont font, String string) {
